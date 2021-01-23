37e026ff1170a4ad7c26c6e867a517d29039017c
Erin Winking <ewink@yahoo.com>
2021-01-22 23:58:43 -0800

    Sign in page created.

--- DashboardApp/urls.py
+++ DashboardApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.display_home, name = 'home'),
+    path('signin', views.display_login, name = 'login'),
+    path('register', views.display_register, name = 'register')
 ]
 
--- DashboardApp/views.py
+++ DashboardApp/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render, redirect
 
-def display_home(request):
-    return render(request, 'html/home.html')
+# GET GET GET GET GET GET GET
 
+def display_home(request):
+    context = {
+        'page_title': 'Welcome To Our Test Page!'
+    }
+    return render(request, 'html/home.html', context)
+
+def display_login(request):
+    context = {
+        'page_title': 'Sign In Here!'
+    }
+    return render(request, 'html/sign_in.html', context)
+
+def display_register(request):
+    context = {
+        'page_title': 'Register For An Account!'
+    }
+    return render(request, 'html/register.html', context)
+
+
+# POST POST POST POST POST POST POST
