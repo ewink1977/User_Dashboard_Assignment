1af947b30d2533bcb0ab821d61ada05f3b686cbe
Erin Winking <ewink@yahoo.com>
2021-01-23 00:43:33 -0800

    MODELS made, MIGRATIONS complete.

--- DashboardApp/models.py
+++ DashboardApp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+class User(models.Model):
+    email = models.EmailField()
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
+    password = models.CharField(max_length = 255)
+    description = models.TextField(default = 'Set Your Description!')
+    user_level = models.IntegerField(default = 1)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+class WallPost(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(
+        User,
+        related_name = 'WallPost',
+        on_delete = CASCADE
+    )
+
+class Comment(models.Model):
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+    user = models.ForeignKey(
+        User,
+        related_name = 'CommentPost',
+        on_delete = CASCADE
+    )
+    message = models.ForeignKey(
+        WallPost,
+        related_name = 'CommentPost',
+        on_delete = CASCADE
+    )
+
